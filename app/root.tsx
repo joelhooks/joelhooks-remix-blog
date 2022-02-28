ad2a485223f790c4ca2877f40f14e2fdbc879833
Joel Hooks <joelhooks@gmail.com>
2022-02-27 18:29:54 -0800

    fix: some broken images

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {Links, Meta} from 'remix'
+import {Links, Meta, MetaFunction, ScrollRestoration} from 'remix'
 import {LiveReload, Outlet} from 'remix'
 
 import styles from './styles/tailwind.css'
@@ -8,10 +8,7 @@ export function links() {
   return [{rel: 'stylesheet', href: styles}]
 }
 
-export const meta: MetaFunction = ({data}) => {
-  const requestInfo = (data as LoaderData | undefined)?.requestInfo
-  const title = "Joel's digital garden"
-  const description = 'Ramblings of a software developer'
+export const meta: MetaFunction = () => {
   return {
     viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
   }
@@ -28,6 +25,7 @@ export default function App() {
       </head>
       <body>
         <Outlet />
+        <ScrollRestoration />
         {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
     </html>
