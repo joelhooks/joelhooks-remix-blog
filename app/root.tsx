5917fd5fe82c559241e4abe1bafea1c3534f0bf0
Joel Hooks <joelhooks@gmail.com>
2022-02-27 18:24:31 -0800

    feat: add screen meta so mobile is scaled right

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {Links} from 'remix'
+import {Links, Meta} from 'remix'
 import {LiveReload, Outlet} from 'remix'
 
 import styles from './styles/tailwind.css'
@@ -8,12 +8,22 @@ export function links() {
   return [{rel: 'stylesheet', href: styles}]
 }
 
+export const meta: MetaFunction = ({data}) => {
+  const requestInfo = (data as LoaderData | undefined)?.requestInfo
+  const title = "Joel's digital garden"
+  const description = 'Ramblings of a software developer'
+  return {
+    viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
+  }
+}
+
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <title>Joel Hooks Digital Gardener</title>
+        <Meta />
         <Links />
       </head>
       <body>
